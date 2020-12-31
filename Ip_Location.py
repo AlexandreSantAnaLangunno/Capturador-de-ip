27f96404595458f7660e597357b40c9da158ac23
Alexandre <71261076+AlexandreSantAnaLangunno@users.noreply.github.com>
2020-12-31 06:42:53 -0300

    Update Ip_Location.py
    Simplificando o código e add interface gráfica com PySimple GUI

--- Ip_Location.py
+++ Ip_Location.py
@@ -1,9 +1,5 @@
 import socket
+from IpFinder_Packges import IpFinder
 
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
-print('')
-print('-------------------------------------' * 2)
-print("Nome do Computador: ", hostname)
-print('-------------------------------------' * 2)
-print('Seu Ip: ', local_ip)
+nome_pc = socket.gethostname()
+IpFinder(nome_pc)
